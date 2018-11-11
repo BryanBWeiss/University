a4946e80a39372a23a15e471126fdf3db5a22f2c
BryanBWeiss <bryan.beltran.g@gmail.com>
2018-11-11 14:56:57 +0100

    agregado

---
+++ numeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zoo_Sistema
+{
+    public partial class numeros : Button
+    {
+
+        public numeros()
+        {
+            base.SetStyle(ControlStyles.Selectable, false);
+            base.UpdateStyles();
+        }
+
+
+    }
+}
